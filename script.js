ce2dfd04590a8c35090e20ad99d627bc53a1646d
Rohit <rsrohit@outlook.com>
2024-01-20 00:02:24 +0100

    2

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     displayMessage('Hey! I am Riddle. Let\'s solve a riddle. Some fun and horror theme.');
+    displayMessage('I am stuck in your device. To escape, enter the master password, a date where people celebrate and give surprises:');
   });
   
-  function displayMessage(message) {
+  function displayMessage(message, addPrompt = true) {
     const terminalOutput = document.getElementById('output');
-    terminalOutput.textContent += `\n> ${message}`;
+    terminalOutput.innerHTML += (addPrompt ? '\n> ' : '\n') + message;
+    terminalOutput.scrollTop = terminalOutput.scrollHeight; // Scroll to bottom
   }
   
   function processCommand(input) {
-    const terminalOutput = document.getElementById('output');
     const trimmedInput = input.trim();
-    if (trimmedInput === '14/03/2023') {
-      displayMessage('Correct! You\'ve escaped the PC!');
+    if (trimmedInput.toLowerCase() === 'clc') {
+      document.getElementById('output').innerHTML = '';
+      displayMessage('Terminal cleared.', false);
+    } else if (trimmedInput === '14/03/2023') {
+      displayMessage('Correct! You\'ve escaped the PC!', false);
     } else {
-      displayMessage('Incorrect. Try again.');
+      displayMessage('Incorrect. Try again.', false);
     }
-    terminalInput.value = ''; // Clear input
-    terminalOutput.scrollTop = terminalOutput.scrollHeight; // Scroll to bottom
   }
   
   const terminalInput = document.getElementById('input');
   terminalInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
+      displayMessage(this.value, false); // Show the user input
       processCommand(this.value);
+      this.value = ''; // Clear input
     }
   });
+  
