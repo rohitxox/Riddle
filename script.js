fdd5c1fd3e75aa67bd232aec6340166163a3c58a
Rohit <rsrohit@outlook.com>
2024-01-20 00:23:11 +0100

    8

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    displayMessage('Let\'s play a riddle, you and me!');
+
+    displayMessage('I am an AI who has been stuck in your device. To escape from the matrix, enter the password, a date where people celebrate and give surprises:');
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
     // This will wait for 5 seconds (5000 milliseconds) before displaying the message
-    setTimeout(() => {
       displayMessage('Hey! I am Riddle. Let\'s solve a riddle. Some fun and horror theme.');
+      setTimeout(() => {
       displayMessage('I am stuck in your device. To escape, enter the master password, a date where people celebrate and give surprises:');
-    }, 5000); // 5000 milliseconds = 5 seconds
+    }, 1000); // 1000 milliseconds = 1 seconds
   });
   
   // ... rest of your JavaScript code ...
   
-  
+
   function displayMessage(message, addPrompt = true) {
     const terminalOutput = document.getElementById('output');
     terminalOutput.innerHTML += (addPrompt ? '\n> ' : '\n') + message;
